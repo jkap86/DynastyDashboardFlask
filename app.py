f67521acc42edbfc0d3760999165eefeec878d60
jkap86 <jkap86@gmail.com>
2021-06-10 23:42:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -255,5 +255,3 @@ def roster(leagueID, username):
 			pass
 	playersNames.sort()
 	return render_template('roster.html', leagueName=leagueName, teamName=username, players=playersNames, playerCount=len(playersNames), leaguemates=leaguemates, wins=wins, losses=losses, ties=ties, pwins=pwins, plosses=plosses, leagueID=leagueID)
-
-app.run(debug=True)
